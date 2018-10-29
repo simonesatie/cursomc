7dafab3cf30a100ce849b584837332abbc0d00a3
Simone Satie <simone.ysayama@gmail.com>
2018-10-28 22:15:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/simonesatie/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.simonesatie.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.simonesatie.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
